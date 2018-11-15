ffb9206f8135099db84962f081b65b5fb10812f6
KobiShpak <kobishpak@gmail.com>
2018-11-15 21:54:45 +0200

    Changed regex for email validation, it used to be okay with a@a

--- app/src/main/java/com/example/kobishpak/hw01/EnterUserInfoActivity.java
+++ app/src/main/java/com/example/kobishpak/hw01/EnterUserInfoActivity.java
@@ -233,7 +233,7 @@ public class EnterUserInfoActivity extends AppCompatActivity {
     }
 
     private boolean isEmailValid(String email) {
-        String regEx="^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$";
+        String regEx="^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])$";
         Pattern pattern = Pattern.compile(regEx);
         Matcher matcher = pattern.matcher(email);
 
@@ -322,7 +322,6 @@ public class EnterUserInfoActivity extends AppCompatActivity {
         }else {
             Toast.makeText(EnterUserInfoActivity.this, "You haven't picked Image",Toast.LENGTH_LONG).show();
             isImageUploaded = false;
-
         }
     }
 }
