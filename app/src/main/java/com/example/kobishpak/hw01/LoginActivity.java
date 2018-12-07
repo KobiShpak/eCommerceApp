60a435372ae628bd5b5c3d0d85ab93aa419c7bba
KobiShpak <kobishpak@gmail.com>
2018-12-07 16:07:59 +0200

    informative toast

--- app/src/main/java/com/example/kobishpak/hw01/LoginActivity.java
+++ app/src/main/java/com/example/kobishpak/hw01/LoginActivity.java
@@ -249,7 +249,7 @@ public class LoginActivity extends AppCompatActivity {
                         } else {
                             // If sign in fails, display a message to the user.
                             Log.w(TAG, "signInWithCredential:failure", task.getException());
-                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                            Toast.makeText(LoginActivity.this, "Authentication failed. Please make sure you have not used the same email with several registration methods",
                                     Toast.LENGTH_SHORT).show();
                         }
                     }
