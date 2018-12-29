87b0cc30d2e4bbfc2ba22f15b49f2bfc9f2fc297
KobiShpak <kobishpak@gmail.com>
2018-12-30 01:12:32 +0200

    Last minor bugs

--- app/src/main/java/com/example/kobishpak/hw01/AllProductsActivity.java
+++ app/src/main/java/com/example/kobishpak/hw01/AllProductsActivity.java
@@ -107,7 +107,7 @@ public class AllProductsActivity extends AppCompatActivity {
             pleaseWait();
             DisplayUserInformation();
 
-            if (m_FirebaseUser.getEmail() == "")
+            if (m_FirebaseUser.getEmail() == null || m_FirebaseUser.getEmail() == "")
             {
                 myUser = new User();
                 getAllBooks();
@@ -326,20 +326,7 @@ public class AllProductsActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-        if (doubleBackToExitPressedOnce) {
             super.onBackPressed();
             return;
-        }
-
-        this.doubleBackToExitPressedOnce = true;
-        Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show();
-
-        new Handler().postDelayed(new Runnable() {
-
-            @Override
-            public void run() {
-                doubleBackToExitPressedOnce=false;
-            }
-        }, 2000);
     }
 }
